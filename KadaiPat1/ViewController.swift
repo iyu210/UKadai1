3b98a4ee5ebb067aa1296e7d054de0f4ccc47314
iyu.swiftLeaner <iyu@live.jp>
2021-10-05 16:16:27 +0900

    SwiftLintの指摘箇所の修正

--- KadaiPat1/ViewController.swift
+++ KadaiPat1/ViewController.swift
@@ -9,62 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet weak private var textField1: UITextField!
+    @IBOutlet weak private var textField2: UITextField!
+    @IBOutlet weak private var textField3: UITextField!
+    @IBOutlet weak private var textField4: UITextField!
+    @IBOutlet weak private var textField5: UITextField!
     
-    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak private var numberLabel: UILabel!
     
     var resultNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func plusButton(_ sender: Any) {
-        
-        if textField1.text?.isEmpty == false{
+    @IBAction private func plusButton(_ sender: Any) {
+        if textField1.text?.isEmpty == false {
             let number: Int? = Int(textField1.text!)!
-            resultNumber = resultNumber + number!
+            resultNumber += number!
         }
         
-        if textField2.text?.isEmpty == false{
+        if textField2.text?.isEmpty == false {
             let number: Int? = Int(textField2.text!)!
-            resultNumber = resultNumber + number!
+            resultNumber += number!
         }
         
-        
-        if textField3.text?.isEmpty == false{
+        if textField3.text?.isEmpty == false {
             let number: Int? = Int(textField3.text!)!
-            resultNumber = resultNumber + number!
+            resultNumber += number!
         }
         
-        
-        if textField4.text?.isEmpty == false{
+        if textField4.text?.isEmpty == false {
             let number: Int? = Int(textField4.text!)!
-            resultNumber = resultNumber + number!
+            resultNumber += number!
         }
         
-        if textField5.text?.isEmpty == false{
+        if textField5.text?.isEmpty == false {
             let number: Int? = Int(textField5.text!)!
-            resultNumber = resultNumber + number!
+            resultNumber += number!
         }
         
         numberLabel.text = String(resultNumber)
         view.endEditing(true)
         resultNumber = 0
-        
     }
     
-    @objc func dismissKeyboard(){
+    @objc func dismissKeyboard() {
         // キーボードを閉じる
         view.endEditing(true)
     }
-    
 }
-
