b83054053099df171c0c598c31f574b76679b0cf
iyu210 <iyu@live.jp>
2022-01-03 09:02:18 +0900

    "A"などを入力して計算するとクラッシュする問題を解決

--- KadaiPat1/ViewController.swift
+++ KadaiPat1/ViewController.swift
@@ -8,57 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak private var textField1: UITextField!
     @IBOutlet weak private var textField2: UITextField!
     @IBOutlet weak private var textField3: UITextField!
     @IBOutlet weak private var textField4: UITextField!
     @IBOutlet weak private var textField5: UITextField!
-    
     @IBOutlet weak private var numberLabel: UILabel!
-    
     var resultNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
-        // Do any additional setup after loading the view.
     }
-    
     @IBAction private func plusButton(_ sender: Any) {
-        if textField1.text?.isEmpty == false {
-            let number: Int? = Int(textField1.text!)!
+        if let numbertText = textField1.text {
+            let number: Int? = (numbertText as NSString).integerValue
             resultNumber += number!
         }
         
-        if textField2.text?.isEmpty == false {
-            let number: Int? = Int(textField2.text!)!
+        if let numbertText = textField2.text {
+            let number: Int? = (numbertText as NSString).integerValue
             resultNumber += number!
         }
         
-        if textField3.text?.isEmpty == false {
-            let number: Int? = Int(textField3.text!)!
+        if let numbertText = textField3.text {
+            let number: Int? = (numbertText as NSString).integerValue
             resultNumber += number!
         }
         
-        if textField4.text?.isEmpty == false {
-            let number: Int? = Int(textField4.text!)!
+        if let numbertText = textField4.text {
+            let number: Int? = (numbertText as NSString).integerValue
             resultNumber += number!
         }
         
-        if textField5.text?.isEmpty == false {
-            let number: Int? = Int(textField5.text!)!
+        if let numbertText = textField5.text {
+            let number: Int? = (numbertText as NSString).integerValue
             resultNumber += number!
         }
-        
         numberLabel.text = String(resultNumber)
         view.endEditing(true)
         resultNumber = 0
     }
-    
     @objc func dismissKeyboard() {
-        // キーボードを閉じる
         view.endEditing(true)
     }
 }
